23604958d8fde2378927ef8626e3a87856f684b9
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-12-31 18:20:02 -0500

    Create Main.java

---
+++ Strings/palindrome/Main.java
@@ -0,0 +1,21 @@
+/*
+* Write a boolean function to determine if a string is a palindrome or not:
+*
+* Examples:
+* Alaska - false
+* dad    - true
+* cat    - false
+* Dad    - true
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static boolean palindrome(String s) {
+
+    }
+}
