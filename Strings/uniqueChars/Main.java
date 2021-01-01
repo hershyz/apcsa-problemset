630fdf479846f00291e1b6ca74aedc83f612442c
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-01 16:47:03 -0500

    Create Main.java

---
+++ Strings/uniqueChars/Main.java
@@ -0,0 +1,27 @@
+/*
+* Write a function that returns the number of unique characters in a string:
+*
+* Examples:
+* aaa    - 1
+* abc    - 3
+* dog    - 3
+* dogg   - 3
+* alaska - 4
+* Alaska - 5
+* aAa    - 2
+* */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static int uniqueChars(String s) {
+
+    }
+}
