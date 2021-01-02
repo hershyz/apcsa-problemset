e9120e25f6a265d7081a83b5e3e7c338c0375076
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-02 12:39:54 -0500

    Create Main.java

---
+++ Arrays and Lists/highestIndex/Main.java
@@ -0,0 +1,20 @@
+/*
+* Write a function that returns the index of the highest value in an array:
+*
+* Examples:
+* [0, 1, 2, 3, 4]     - 4
+* [10, 9, 9, 7, 8]    - 0
+* [19, 100, 110, 102] - 2
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static int highestIndex(int[] arr) {
+        
+    }
+}
