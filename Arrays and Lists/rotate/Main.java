3e6005f33edc6ac0be0ca7cd3ff2ec85e25d5037
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-02 13:51:29 -0500

    Create Main.java

---
+++ Arrays and Lists/rotate/Main.java
@@ -0,0 +1,19 @@
+/*
+* Write a function that rotates an array towards the right:
+*
+* Examples:
+* [0, 1, 2, 3, 4]   - [4, 0, 1, 2, 3]
+* [10, 9, 8, 7, 6]  - [6, 10, 9, 8, 7]
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static int[] rotate(int[] arr) {
+        return arr;
+    }
+}
