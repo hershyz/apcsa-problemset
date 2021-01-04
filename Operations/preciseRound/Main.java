7e88a5bab3238489e6d882b8710bc4e167d9c2ed
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-04 18:43:04 -0500

    Create Main.java

---
+++ Operations/preciseRound/Main.java
@@ -0,0 +1,20 @@
+/*
+* Write a function that rounds a positive double value to the nearest tenth and returns the value:
+*
+* Examples:
+* 10.1    - 10.1
+* 10.15   - 10.2
+* 110.23  - 110.2
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static double presiceRound(double n) {
+
+    }
+}
