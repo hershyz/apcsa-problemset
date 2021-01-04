6be36e63f5942f1f4a823d3b5d645cca5b807534
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-04 14:40:11 -0500

    Create Main.java

---
+++ Arrays and Lists/Mean/Main.java
@@ -0,0 +1,20 @@
+/*
+* Write a function that returns the mean value of all elements in an array:
+*
+* Examples:
+* [1, 2, 3, 4, 5]  - 3
+* [12, 2]          - 7
+* [2, 2, 3, 3]     - 2.5
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static double mean(int[] arr) {
+
+    }
+}
