6d695972845c6008d297b9908058931491241f18
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-06 16:36:48 -0500

    Create Main.java

---
+++ Arrays and Lists/rotate2d/Main.java
@@ -0,0 +1,26 @@
+/*
+* Write a function that rotates a 2d array:
+*
+* Input:
+* [1, 2, 3, 4
+*  5, 6, 7, 8
+*  9, 10, 11, 12]
+*
+* Result:
+* [1, 5, 9
+*  2, 6, 10
+*  3, 7, 11
+*  4, 8, 12]
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static int[][] rotate2d(int[][] arr) {
+
+    }
+}
