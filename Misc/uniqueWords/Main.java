b4084cdb8bb7f50633e7ec99f67deb8052634d71
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-05 21:39:50 -0500

    Create Main.java

---
+++ Misc/uniqueWords/Main.java
@@ -0,0 +1,19 @@
+/*
+* Return a string array of all the unique words in a string without the use of ArrayLists:
+*
+* Givens:
+* There will be at least 2 words in the string separated by a space character.
+* Words are case sensitive, uppercase/lowercase characters are considered unique regardless of spelling.
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static String[] uniqueWords(String s) {
+
+    }
+}
