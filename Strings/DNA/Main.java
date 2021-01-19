a3765a1fce8a35f63815de5329722f88cf3ea227
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-18 23:09:50 -0500

    Create Main.java

---
+++ Strings/DNA/Main.java
@@ -0,0 +1,23 @@
+/*
+* Write a function that finds the corresponding strand of base pairs to a strand of DNA without using the String.replace() function:
+*
+* Givens:
+* A and T are corresponding
+* C and G are corresponding
+*
+* Examples:
+* ATCG -> TAGC
+* GGAT -> CCTA
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static String correspondingDNAStrand(String sequence) {
+        
+    }
+}
