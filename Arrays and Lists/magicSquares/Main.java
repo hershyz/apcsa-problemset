647734b68ec7bcc5d461110e31b7e4c00fd3d362
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-29 00:06:01 -0500

    Create Main.java

---
+++ Arrays and Lists/magicSquares/Main.java
@@ -0,0 +1,16 @@
+/*
+* Write a function that determines if a square 2D integer array is "magic" or not.
+* A 2D array is "magic" if every row, column, and diagonal has the same sum.
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static boolean isMagic(int[][] arr) {
+
+    }
+}
