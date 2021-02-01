ea91c003dda49993c69d8d8ec5835f6b07ee7194
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-01-31 22:31:03 -0500

    Create Main.java

---
+++ Arrays and Lists/reverse/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Given an array, reverse it and return it:
+ *
+ * Examples:
+ * [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+ * */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static int[] reverse(int[] arr) {
+
+    }
+}
