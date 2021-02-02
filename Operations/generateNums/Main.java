9419c9c49ff353452cfcf4f1b8c20d31b4a8f361
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-02-01 23:17:17 -0500

    Create Main.java

---
+++ Operations/generateNums/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Given a mean m, return a list of n unique values which have a mean m:
+ * Example:
+ * genetateNums(6, 3) -> [5, 6, 7]
+ * generateNums(6, 4) -> [4, 5, 7, 8]
+ * */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    //Solution:
+    public static List<Integer> generateNums(int m, int n) {
+        
+    }
+}
