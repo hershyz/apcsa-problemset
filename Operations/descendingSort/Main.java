fe25401e9c6ce3b8dfcbf43fe93cb27e48b59007
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-02-17 01:56:48 -0500

    Create Main.java

---
+++ Operations/descendingSort/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Given an array of integers, sort it in descending order and return the result.
+ *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+
+    }
+
+    public static int[] descendingSort(int[] arr) {
+
+    }
+}
