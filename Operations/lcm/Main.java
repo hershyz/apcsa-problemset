42224da78dd64267a6605336da3664c990b99324
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-03-21 23:35:34 -0400

    Create Main.java

---
+++ Operations/lcm/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Given two integers, find their lowest common multiple:
+ *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    public static int lcm(int a, int b) {
+
+    }
+}
