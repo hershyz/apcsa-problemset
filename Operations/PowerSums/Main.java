f3e4401fc55c96d83a83b2135b6be1adf4e8f24b
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-03-21 23:48:35 -0400

    Create Main.java

---
+++ Operations/PowerSums/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Given integers a and b, return the sum of a^b + b^a
+ * Do not use the built in Math.pow() function, use a loop to calculate powers instead
+ *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Check.check();
+    }
+
+    public static int powerSum(int a, int b) {
+
+    }
+}
