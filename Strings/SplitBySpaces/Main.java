ce788fb7c5fe828844c112873b1a4d5386fba599
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-03-22 00:43:45 -0400

    Create Main.java

---
+++ Strings/SplitBySpaces/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Given a string, split it into an array by spaces without using the built in String.split() function in Java.
+ * You are allowed to use String.charAt() in your solution.
+ * For example:
+ * "Hello, how are you" should return ["Hello,", "how", "are", "you"]
+ *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    public static String[] splitBySpaces(String s) {
+        
+    }
+}
