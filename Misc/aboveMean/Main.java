cb71d7a11b4b7cf0bdad732ca4536df7b2d098d4
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-03-21 23:58:16 -0400

    Create Main.java

---
+++ Misc/aboveMean/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Given an double[] array, return the number of values within the array that are above its mean.
+ *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Check.check();
+    }
+
+    public static int aboveMean(double[] arr) {
+
+    }
+}
