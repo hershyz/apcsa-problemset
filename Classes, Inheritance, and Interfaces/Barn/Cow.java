8d449bacfc0c0e61591ef41d9a035cc07aee4ebf
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-06 01:04:14 -0400

    Create Cow.java

---
+++ Classes, Inheritance, and Interfaces/Barn/Cow.java
@@ -0,0 +1,12 @@
+public class Cow extends Animal {
+
+    //Use the super() keyword to trigger the parent constructor:
+    public Cow(String name, int age, int height) {
+        
+    }
+
+    //Return "Moo!" when this function is called:
+    public String getSound() {
+        
+    }
+}
