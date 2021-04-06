cd20b3a6f618c2a7848275f1e667dda14bef75aa
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-05 23:59:46 -0400

    Create Main.java

---
+++ Recursion/Pow/Main.java
@@ -0,0 +1,14 @@
+/*
+* Given a and b, find the result of a ^ b using a recursive function.
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+
+    public static int pow(int a, int b) {
+        
+    }
+}
