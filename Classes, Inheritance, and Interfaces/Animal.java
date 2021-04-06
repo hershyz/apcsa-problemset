76f43452708a03af7729c781d383b6eb7b48586f
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-06 01:01:57 -0400

    Update Animal.java

--- Classes, Inheritance, and Interfaces/Animal.java
+++ Classes, Inheritance, and Interfaces/Animal.java
@@ -6,23 +6,21 @@ public class Animal {
 
     //Initialize name, age, and height to the parameters:
     public Animal(String name, int age, int height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
+        
     }
 
     //Return the name:
     public String getName() {
-        return name;
+
     }
 
     //Return the age:
     public int getAge() {
-        return age;
+        
     }
 
     //Return the height:
     public int getHeight() {
-        return height;
+        
     }
 }
