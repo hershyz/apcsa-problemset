c5fe2cd8fdbf570fbc4bf72009b2a1b3dc12dad1
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-07 20:11:16 -0400

    Update Cow.java

--- Classes, Inheritance, and Interfaces/Cow Interface/Cow.java
+++ Classes, Inheritance, and Interfaces/Cow Interface/Cow.java
@@ -4,26 +4,18 @@ public class Cow implements Animal {
 
     //Set 'foods' equal to 'acceptableFoods'
     public Cow(String[] acceptableFoods) {
-        foods = new String[acceptableFoods.length];
-        for (int i = 0; i < acceptableFoods.length; i++) {
-            foods[i] = acceptableFoods[i];
-        }
+        
     }
 
     public String makeNoise() {
-        return "Moo!";
+        
     }
 
     public boolean eat(String food) {
-        for (String x : foods) {
-            if (x.equals(food)) {
-                return true;
-            }
-        }
-        return false;
+
     }
 
     public String[] getFoods() {
-        return foods;
+        
     }
 }
