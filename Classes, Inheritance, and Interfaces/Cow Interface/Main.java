9477e69839477eef5da00fe74040e8728baa85c7
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-07 20:10:34 -0400

    Create Main.java

---
+++ Classes, Inheritance, and Interfaces/Cow Interface/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /*
+    * Write the Cow.java class, using the 'Animal' interface and the voids it is supposed to implement.
+    * Write the Cow.Cow() constructor in the 'Cow' class, transferring the String data in the array parameter to the class-level variable.
+    * */
+
+    public static void main(String[] args) {
+        Check.check();
+    }
+}
